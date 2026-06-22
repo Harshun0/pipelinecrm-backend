d4b354d8b5375d0690e04f64cea4d4e9aec0e121
Harsh Nebhnani <harshnebhnani02@gmail.com>
2026-06-22 17:30:11 +0530

    fix

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:8000",
   "http://localhost:3000",
+  "https://pipelinescrm.vercel.app",
   "http://localhost:3001",
 ].filter(Boolean);
 
